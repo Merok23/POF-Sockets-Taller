7cc39557edead33dcb3362489507c02b5669414e
Merok23 <alejocalizblanco@gmail.com>
2023-04-02 01:24:34 -0300

    voy a llorar

--- src/common_socket.cpp
+++ src/common_socket.cpp
@@ -2,104 +2,97 @@
 
 //Socket is heavily based on the class given material, if I had more time, it would be more original
 
-class Socket {
-private:
-    int file_descriptor;//this is the file descriptor of the socket
-    bool connected;//this is a flag that indicates if the socket is open or not (if it is connected or not)(open is connected)
+// if it has 2 parameters it will create a socket and try to connect to the ip and port given
+Socket::Socket(const char* ip, const char* port){
+    Resolver resolver(ip, port, false); //false because it is not passive, its trying to connect to the server.
+    int error = -1;
+    int fd = -1;
+    this->connected = false;
+    
+    while(resolver.hasNext() && !this->connected){
+        struct addrinfo* address = resolver.next();
+        fd = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
+        if(fd == -1) continue;
 
-public:
-    // if it has 2 parameters it will create a socket and try to connect to the ip and port given
-    Socket::Socket(const char* ip, const char* port){
-        Resolver resolver(ip, port, false); //false because it is not passive, its trying to connect to the server.
-        int error = -1;
-        int fd = -1;
-        this->connected = false;
-        
-        while(resolver.hasNext() && !this->connected){
-            struct addrinfo* address = resolver.next();
-            fd = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
-            if(fd == -1) continue;
-
-            error = connect(fd, address->ai_addr, address->ai_addrlen);
-            if(error == -1){
-                close(fd);
-                continue;
-            }
+        error = connect(fd, address->ai_addr, address->ai_addrlen);
+        if(error == -1){
+            close(fd);
+            continue;
         }
-        this->connected = true;
-        this->file_descriptor = fd;
     }
+    this->connected = true;
+    this->file_descriptor = fd;
+}
 // if it only has a port it will create a socket and try to listen to the port given
-    Socket::Socket(const char* port){
-        Resolver resolver(nullptr, port, true); //true because it is passive, its waiting for a connection
-        int error = -1;
-        int fd = -1;
+Socket::Socket(const char* port){
+    Resolver resolver(nullptr, port, true); //true because it is passive, its waiting for a connection
+    int error = -1;
+    int fd = -1;
+    this->connected = false;
+    while(resolver.hasNext()){
+        struct addrinfo *address = resolver.next();
+        fd = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
+        if(fd == -1) continue;
+
+        //If I'm here it's because the socket was created successfully
+        error = bind(fd, address->ai_addr, address->ai_addrlen);
+        if(error == -1){
+            close(fd);
+            continue;
+        }
+        error = listen(fd, 10); //10 is the maximum number of connections that can be waiting to be accepted
+        if(error == -1){
+            close(fd);
+            continue;
+        }
+    }
+    //If I'm here it's because the socket was created successfully and it's listening
+    this->connected = true;
+    this->file_descriptor = fd;
+    return;
+}
+
+int Socket::sendData(const char* data, size_t size){
+    int size_sent = 0;
+    while(size_sent < size){
+        int sent = send(this->file_descriptor, data + size_sent, size - size_sent, 0);
+        if(sent == -1){
+            return -1; //exceptions missing
+        }
+        size_sent += sent;
+    }
+    return 0;
+}
+
+int Socket::receiveData(char* buffer, size_t size){
+    int size_received = 0;
+    while(size_received < size){
+        int received = recv(this->file_descriptor, buffer + size_received, size - size_received, 0);
+        if(received == -1){
+            return -1; //exceptions missing
+        }
+        size_received += received;
+    }
+    return 0;
+}
+
+int Socket::disconnect(){
+    if(this->connected){
+        int error = shutdown(this->file_descriptor, SHUT_RDWR);
+        if(error == -1){
+            return -1; //exceptions missing
+        }
+        close(this->file_descriptor);
         this->connected = false;
-        while(resolver.hasNext()){
-            struct addrinfo *address = resolver.next();
-            fd = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
-            if(fd == -1) continue;
-
-            //If I'm here it's because the socket was created successfully
-            error = bind(fd, address->ai_addr, address->ai_addrlen);
-            if(error == -1){
-                close(fd);
-                continue;
-            }
-            error = listen(fd, 10); //10 is the maximum number of connections that can be waiting to be accepted
-            if(error == -1){
-                close(fd);
-                continue;
-            }
-        }
-        //If I'm here it's because the socket was created successfully and it's listening
-        this->connected = true;
-        this->file_descriptor = fd;
-        return;
     }
+    return 0;
+}
 
-    int Socket::sendData(const char* data, size_t size){
-        int size_sent = 0;
-        while(size_sent < size){
-            int sent = send(this->file_descriptor, data + size_sent, size - size_sent, 0);
-            if(sent == -1){
-                return -1; //exceptions missing
-            }
-            size_sent += sent;
-        }
-        return 0;
+Socket::~Socket(){
+    if(this->connected){
+        Socket::disconnect();
     }
-
-    int Socket::receiveData(char* buffer, size_t size){
-        int size_received = 0;
-        while(size_received < size){
-            int received = recv(this->file_descriptor, buffer + size_received, size - size_received, 0);
-            if(received == -1){
-                return -1; //exceptions missing
-            }
-            size_received += received;
-        }
-        return 0;
-    }
-
-    int Socket::disconnect(){
-        if(this->connected){
-            int error = shutdown(this->file_descriptor, SHUT_RDWR);
-            if(error == -1){
-                return -1; //exceptions missing
-            }
-            close(this->file_descriptor);
-            this->connected = false;
-        }
-        return 0;
-    }
-
-    Socket::~Socket(){
-        this->disconnect();
-    }
-
-};
-
+}
 /* Esto es algo que estaba tratanod de hacer pero no me funciono
 
 
--- src/common_socket.h
+++ src/common_socket.h
@@ -1,17 +1,36 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "common_resolver.h"
 
 
 class Socket {
 private:
-    int file_descriptor;
-    bool connected;
+    int file_descriptor;//this is the file descriptor of the socket
+    bool connected;//this is a flag that indicates if the socket is open or not (if it is connected or not)(open is connected)
+
 public:
-    Socket(const char* ip, int port);
+    Socket(const char* ip, const char* port);//for connecting
+
+    Socket(const char* port);//for listening
     
     int sendData(const char* data, size_t size);
     
     int receiveData(char* buffer, size_t size);
 
+    int disconnect();
+
     ~Socket();
 };
+
+#endif
